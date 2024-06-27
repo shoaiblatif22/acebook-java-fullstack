8eb82ad1e609ec9ec376922da8641c74f7888e58
Shoaib Latif <shoaiblatif11@gmail.com>
2024-06-27 21:05:14 +0100

    New config files to enable react unit testing

---
+++ my-frontend/babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    presets: [
+
+        [
+
+            '@babel/preset-env',
+
+            {
+
+                targets: {
+
+                    node: 'current',
+
+                },
+
+            },
+
+        ],
+
+        '@babel/preset-react', // Adds support for JSX
+
+    ],
+
+    plugins: [],
+
+};
--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1>learn react</h1>
       </header>
     </div>
   );
